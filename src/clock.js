688a95c22eafc3a9924140db3d310bcd0045b81f
Nic Gill <31864550+seakeal@users.noreply.github.com>
2023-01-10 11:20:26 -0500

    WIP: Reformatting with grid layout

--- src/clock.js
+++ src/clock.js
@@ -1,18 +1,29 @@
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 document.addEventListener('DOMContentLoaded', function(event) {
     console.log('Clock function loaded');
+    var clock = document.getElementById('clock');
     updateClock();
 });
 
 function updateClock() {
-    const DAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
     var t = new Date();
-    var dateString = DAYS[t.getDay()]+' ' +
-        ('0'+(t.getMonth()+1)).slice(-2)+'.' +
-        ('0'+t.getDate()).slice(-2)+'.' +
-        t.getFullYear()+' '+
-        ('0'+t.getHours()).slice(-2)+':'+
-        ('0'+t.getMinutes()).slice(-2)+':'+
-        ('0'+t.getSeconds()).slice(-2);
-    document.getElementById('time').innerHTML = dateString;
+    // var dateString = DAYS[t.getDay()]+' ' +
+    //     ('0'+(t.getMonth()+1)).slice(-2)+'.' +
+    //     ('0'+t.getDate()).slice(-2)+'.' +
+    //     t.getFullYear()+' '+
+    //     ('0'+t.getHours()).slice(-2)+':'+
+    //     ('0'+t.getMinutes()).slice(-2)+':'+
+    //     ('0'+t.getSeconds()).slice(-2);
+    var dateString = '</div><div id="clockDate" class="time">'+
+    DAYS[t.getDay()]+', '+
+    MONTHS[t.getMonth()] + ' '+
+    ('0'+t.getDate()).slice(-2)+' ' +
+    t.getFullYear()+'</div>' +
+    '<div id="clockTime" class="time">'+
+    ('0'+t.getHours()).slice(-2)+':'+
+    ('0'+t.getMinutes()).slice(-2);
+    clock.innerHTML = dateString;
     setTimeout(updateClock, 1000);
 }
